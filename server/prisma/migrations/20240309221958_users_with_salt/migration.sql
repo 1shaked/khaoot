a3af7d4f4e33551a54ad13e3aa25a499b9c0a808
1shaked <shaked1hen@gmail.com>
2024-03-10 00:20:38 +0200

    Add salt column to User table

---
+++ server/prisma/migrations/20240309221958_users_with_salt/migration.sql
@@ -0,0 +1,19 @@
+/*
+  Warnings:
+
+  - Added the required column `salt` to the `User` table without a default value. This is not possible if the table is not empty.
+
+*/
+-- RedefineTables
+PRAGMA foreign_keys=OFF;
+CREATE TABLE "new_User" (
+    "email" TEXT NOT NULL PRIMARY KEY,
+    "password" TEXT NOT NULL,
+    "salt" TEXT NOT NULL
+);
+INSERT INTO "new_User" ("email", "password") SELECT "email", "password" FROM "User";
+DROP TABLE "User";
+ALTER TABLE "new_User" RENAME TO "User";
+CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
+PRAGMA foreign_key_check;
+PRAGMA foreign_keys=ON;
