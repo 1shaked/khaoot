227b58fb598dadbe92fe6b975754ce1c76707708
1shaked <40497915+1shaked@users.noreply.github.com>
2024-03-10 19:32:24 +0200

    Add token authentication and password hashing

--- server/src/Auth/auth.py
+++ server/src/Auth/auth.py
@@ -1,6 +1,8 @@
 from prisma import Prisma
 from pydantic import BaseModel
 from fastapi import APIRouter
+from fastapi import FastAPI, Request, HTTPException, Depends
+
 # import password as ps
 
 class UserModel(BaseModel):
@@ -27,7 +29,7 @@ def hash_password(password: str) -> (str, bytes): # type: ignore
     pwdhash = hashlib.pbkdf2_hmac('sha256', password.encode('utf-8'), salt, 100000)
     
     # Return the hexadecimal representation of the hash along with the salt
-    return pwdhash.hex(), salt
+    return pwdhash.hex(), salt.hex()
 
 def verify_password(stored_password_hash: str, salt: bytes, provided_password: str) -> bool:
     """Verify a provided password against the stored hash and salt."""
@@ -48,7 +50,9 @@ async def signup(user: UserModel):
     # modify the password to be hashed
     # ps.hash_password(user_json)
     hashed_password, salt = hash_password(user_json['password'])
+    import pdb; pdb.set_trace()
     user_json['password'] = hashed_password
+    user_json['salt'] = salt
     print("user_json", user_json, salt)
     user_db = await db.user.create(data=user_json)
     await db.disconnect()
@@ -57,8 +61,47 @@ async def signup(user: UserModel):
 
 @router.post("/auth/login")
 async def login(user: UserModel):
+    # take the user email and password
+    
+    # find the user in the db
+    db = Prisma()
+    await db.connect()
+    db_user = await db.user.find_unique(where={"email": user.email})
+    await db.disconnect()
+    db_user = db_user.model_dump()
+    print("db_user", db_user)
+    # compare the password with the hashed password
+    db_user['salt']
+    is_login = verify_password(db_user['password'], bytes.fromhex(db_user['salt']), user.password)
+    print(is_login)
+    # if login create a new token
+    if (is_login): 
+        await db.connect()
+        # db_user = await db.token.find_unique(where={"email": user.email})
+        # db.token.update_or_create(data={"token": "123", "user_email": user.email})
+        await db.disconnect()
     return user
 
 @router.post("/auth/logout")
 async def logout(token: TokenModel):
     return token
+
+
+async def token_auth(request: Request):
+    # Check if the token is present in the headers
+    token = request.headers.get("Authorization")
+
+    # If the token is not present, raise an HTTPException
+    if not token:
+        raise HTTPException(status_code=401, detail="Token is missing")
+
+    # If the token is present, check if valid
+    # if not valid, raise an HTTPException
+    db = Prisma()
+    await db.connect()
+    db_token = await db.token.find_unique(where={"token": token})
+    await db.disconnect()
+    if not db_token:
+        raise HTTPException(status_code=401, detail="Token is invalid")
+    
+
