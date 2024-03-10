eaa8c29a9888dcb5ada6f5f989dcee89fb304b0a
1shaked <40497915+1shaked@users.noreply.github.com>
2024-03-11 00:31:48 +0200

    Add dependencies and routes for login and home pages

--- khooat/src/App.tsx
+++ khooat/src/App.tsx
@@ -1,9 +1,16 @@
+import { BrowserRouter, Route, Routes } from 'react-router-dom'
 import './App.css'
 
 function App() {
 
   return (
     <>
+    <BrowserRouter>
+      <Routes>
+          <Route path="/login" Component={() => <div>login</div>}  /> {/* 👈 Renders at /app/ */}
+          <Route path="/" Component={() => <div>home</div>}  /> {/* 👈 Renders at /app/ */}
+      </Routes>
+    </BrowserRouter>
       
     </>
   )
